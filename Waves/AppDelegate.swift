8b93107d984766ff8d8f76941be2d1fa64958566
Connor Douglas <cjdouglas96@gmail.com>
2019-01-13 11:04:31 -1000

    Initial Commit

---
+++ Waves/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Waves
+//
+//  Created by Connor Douglas on 1/13/19.
+//  Copyright © 2019 Connor Douglas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Waves/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Waves
+//
+//  Created by Connor Douglas on 1/13/19.
+//  Copyright © 2019 Connor Douglas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
